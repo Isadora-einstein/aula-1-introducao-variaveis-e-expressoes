fec304d69d93cd7424f31b22d0291aa67fc7311f
Isadora-einstein <163673535+Isadora-einstein@users.noreply.github.com>
2024-03-16 15:17:57 -0300

    Testando o 2

--- exercicio_2.py
+++ exercicio_2.py
@@ -14,3 +14,7 @@ Resposta:
 
 O preço final do produto é 5.25
 """
+preco_do_produto = float (input("digite o preço do produto"))
+desconto = float (input ("digite o desconto(entre 0 e 100)"))
+valor_de_desconto = preco_do_desconto *(desconto/100)
+valor_final = preco_do_produto - valor_do_desconto print (f"O preço final do produto é {valor_final}")
